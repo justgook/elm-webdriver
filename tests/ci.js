e4c0978bac2cc092c0acae0becee3aeaffa7d8b5
Romans Potasovs <rpotasovs@evolutiongaming.com>
2018-09-11 12:55:53 +0300

    Update stats collectiong script

--- tests/ci.js
+++ tests/ci.js
@@ -1,26 +1,28 @@
 const fs = require('fs');
+const path = require('path');
 global.XMLHttpRequest = require("xhr2");
 const app = require("./bundle.js").Elm.Main.init({});
 
-
-// insert = function (index, items) { this.splice.apply(this, [index, 0].concat(items)); }
-
 app.ports.log.subscribe((a) => process.stdout.write(a));
-app.ports.result.subscribe((report) =>
-  fs.readFile('stats.json', 'utf8', function readFileCallback(err, input) {
-    const driver = process.env.DRIVER_NAME || "Unknown Driver2";
-    let stats = err ? { drivers: [], data: {} } : JSON.parse(input);
-    let index = stats.drivers.indexOf(driver);
-    if (index === -1) {
-      index = stats.drivers.push(driver) - 1;
-    }
-    stats.data = Object.keys(report)
-      .reduce((acc, key) => {
-        if (!acc[key]) { acc[key] = [] }
-        acc[key][index] = report[key];
-        return acc
-      }, stats.data);
-    // process.exitCode = errors.length
-    fs.writeFileSync('stats.json', JSON.stringify(stats), 'utf8'/*, callback*/);
-  })
-)
+app.ports.result.subscribe((report) => {
+  const driver = process.env.DRIVER_NAME || "Unknown Driver2";
+  const file = path.resolve(__dirname, 'stats.json')
+  const stats = fs.existsSync(file)
+    ? JSON.parse(fs.readFileSync('stats.json', 'utf8'))
+    : { drivers: [], data: {} };
+
+
+  let index = stats.drivers.indexOf(driver);
+  if (index === -1) {
+    index = stats.drivers.push(driver) - 1;
+  }
+
+  stats.data = Object.keys(report)
+    .reduce((acc, key) => {
+      if (!acc[key]) { acc[key] = [] }
+      acc[key][index] = report[key];
+      return acc
+    }, stats.data);
+
+  fs.writeFileSync(file, JSON.stringify(stats), 'utf8');
+})
